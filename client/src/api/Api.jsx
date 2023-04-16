09e94be966c5b09cce043dc058ebee8be507a0e7
michael valentine <37175136+chionye@users.noreply.github.com>
2023-04-16 15:26:30 +0100

    Rollback change to api.js

--- client/src/api/Api.jsx
+++ client/src/api/Api.jsx
@@ -1,13 +1,5 @@
 import axios from 'axios';
 
-const app = axios.create({
-    baseURL: 'https://paper-mache.herokuapp.com/',
-    headers: {
-        'Access-Control-Allow-Origin': '*'
-    },
-    withCredentials: true
-})
-
 export const Api = async (payload) => {
-    return await app.post('/scrape', payload)
+    return await axios.post('https://paper-mache.herokuapp.com/scrape', payload)
 }
