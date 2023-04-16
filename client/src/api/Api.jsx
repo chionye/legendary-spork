89579f03fd3ddae0ecd33682c9e5b2bbcd9e1b69
michael valentine <37175136+chionye@users.noreply.github.com>
2023-04-16 15:15:36 +0100

    Update Api.jsx

--- client/src/api/Api.jsx
+++ client/src/api/Api.jsx
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+const app = axios.create({
+    baseURL: 'https://paper-mache.herokuapp.com/',
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    },
+    withCredentials: true
+})
+
 export const Api = async (payload) => {
-    return await axios.post('https://paper-mache.herokuapp.com/scrape', payload)
+    return await app.post('https://paper-mache.herokuapp.com/scrape', payload)
 }
